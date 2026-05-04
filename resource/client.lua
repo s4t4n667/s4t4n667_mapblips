d12de4283e0b542d7bbf01ae62b80c9e3187bd0b
Asgaard Developments <146543978+s4t4n667@users.noreply.github.com>
2026-05-04 18:08:27 +0100

    Update blip creation logic for multiple coordinates
    Refactor blip creation to handle multiple coordinates.

--- resource/client.lua
+++ resource/client.lua
@@ -1,16 +1,23 @@
 local config = lib.require('config')
 
 CreateThread(function()
-	Wait(100) 
-	for _, info in pairs(config.blips) do
-		local blip = AddBlipForCoord(info.coords.x, info.coords.y, info.coords.z) 
-		SetBlipSprite(blip, info.sprite)
-		SetBlipDisplay(blip, 4)
-		SetBlipScale(blip, info.scale)
-		SetBlipColour(blip, info.colour)
-		SetBlipAsShortRange(blip, true)
-		BeginTextCommandSetBlipName("STRING")
-		AddTextComponentString(info.label)
-		EndTextCommandSetBlipName(blip)
-	end
+    for _, info in ipairs(config.blips) do
+        local coordsList = info.coords
+
+        if coordsList.x then
+            coordsList = { coordsList }
+        end
+
+        for _, coords in ipairs(coordsList) do
+            local blip = AddBlipForCoord(coords.x, coords.y, coords.z)
+            SetBlipSprite(blip, info.sprite)
+            SetBlipDisplay(blip, 4)
+            SetBlipScale(blip, info.scale)
+            SetBlipColour(blip, info.colour)
+            SetBlipAsShortRange(blip, true)
+            BeginTextCommandSetBlipName("STRING")
+            AddTextComponentString(info.label)
+            EndTextCommandSetBlipName(blip)
+        end
+    end
 end)
